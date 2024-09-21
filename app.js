e6c78d7cab95b59db1bfbe71ea0447d5c0447cc1
rich <dankwarrior6900@gmail.com>
2024-09-20 21:36:17 -0700

    Create getComputerChoice function to a string value

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 "use strict"
 
-console.log("Hello");
+/*
+
+The game is played in the console against a computer.
+The user will pick their choice of rock, paper, or scissors as well as the computer.
+Conditions for winning/losing are dependent on the choice made by the user and computer.
+
+Computer will be a function that is called each time a user decides to play a game.
+Computer will randomly pick between the three choices.
+Computer will return the choice it has made based off of its random choice.
+
+
+0 - 0.99 = 0
+1 - 1.99 = 1
+2 - 2.99 = 2
+
+0 - 0.33
+0.34 - 0.66
+0.67 - 99
+
+*/
+
+function getComputerChoice() {
+    let compChoice;
+    compChoice = Math.floor(Math.random() * 3) + 1;
+    
+    if (compChoice === 1) {
+        return "Rock";
+    } else if (compChoice === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    };
+}
